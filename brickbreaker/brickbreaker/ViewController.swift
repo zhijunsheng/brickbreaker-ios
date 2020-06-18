795195dec043960ff016c9104f4c7d364947818f
ethan <ethanyiyaozhang@gmail.com>
2020-06-18 20:22:32 +0800

    We add 4 directions.

--- brickbreaker/brickbreaker/ViewController.swift
+++ brickbreaker/brickbreaker/ViewController.swift
@@ -1,20 +1,45 @@
-//
-//  ViewController.swift
-//  brickbreaker
-//
-//  Created by daobao on 2020/6/7.
-//  Copyright © 2020 daobao. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var boardView: BoardView!
+    
+
+    var direction: Int = 1 // 3:向左 2:向右 1:向上 0:向下
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+            
+            if self.direction == 3 {
+                self.boardView.ballX -= 10
+                if self.boardView.ballX < 100 {
+                    self.direction = 2
+                }
+            }
+            
+            if self.direction == 2 {
+                self.boardView.ballX += 10
+                if self.boardView.ballX > 600 {
+                    self.direction = 3
+                }
+            }
+            
+            if self.direction == 1 {
+                self.boardView.ballY -= 10
+                if self.boardView.ballY < 100 {
+                    self.direction = 0
+                }
+            }
+            
+            if self.direction == 0 {
+                self.boardView.ballY += 10
+                if self.boardView.ballY > 800 {
+                    self.direction = 1
+                }
+            }
+            self.boardView.setNeedsDisplay()
+        }
     }
-
-
 }
-
